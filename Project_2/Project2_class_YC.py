a75eb0e452fc3bdcdb3ec83c392e0ef0d368316f
YefridC09 <yacordob@ncsu.edu>
2026-03-17 10:58:11 -0400

    Class instances creation methods completed

--- Project_2/Project2_class_YC.py
+++ Project_2/Project2_class_YC.py
@@ -9,12 +9,31 @@ import pandas as pd
 
 class SparkDataCheck:
     """
-    
+    Data quality class 
     """
     
     def __init__(self, df):
         
         self.df = df
+        
+        
+    #Method for creating an instance of the class from a csv file
+    @classmethods
+    def createfrom_csv(cls, spark, file_path):
+        df = spark.read.load(file_path,
+                                format="csv", 
+                                 sep=",", 
+                                 inferSchema="true", 
+                                 header="true")
+        return cls(df)
+    
+    
+    #Method for creating an instance of the class from a pandas dataframe
+    @classmethods
+    def createfrom_pandas(cls, spark, pd_df: pd.DataFrame):
+        df = spark.CreateDataFrame(pd_df)
+        return cls(df)
+    
     
 
     

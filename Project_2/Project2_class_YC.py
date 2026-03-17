74b4de253604e142fbc80ec727d4647886112d79
YefridC09 <yacordob@ncsu.edu>
2026-03-17 17:56:18 -0400

    Completed first validation, need to add description

--- Project_2/Project2_class_YC.py
+++ Project_2/Project2_class_YC.py
@@ -19,7 +19,7 @@ class SparkDataCheck:
         
     #Method for creating an instance of the class from a csv file
     @classmethods
-    def createfrom_csv(cls, spark, file_path):
+    def createfrom_csv(cls, spark, file_path: str):
         df = spark.read.load(file_path,
                                 format="csv", 
                                  sep=",", 
@@ -34,6 +34,34 @@ class SparkDataCheck:
         df = spark.CreateDataFrame(pd_df)
         return cls(df)
     
+    #Validation methods creation
     
-
-    
+    def create_range_boolean(self, column, lower = None, upper = None):
+        dict_dtypes = dict(self.df.dtypes)
+        col_type = dict_dtypes.get(column)
+        
+        num_types = ['float', 'int', 'longint', 'bigint', 'double', 'integer']
+        
+        if col_type is not None and col_type.lower() in num_types:
+            if lower is not None and upper is not None:
+                self.df = self.df.withColumn("Is_in_range", 
+                   when((self.df[column]>=lower & self.df[column]<=upper), True)
+                  .when(self.df[column].isNull(), F.lit(None).cast('boolean'))
+                  .otherwise(False))
+                return self.df
+            elif lower is not None and upper is None:
+                self.df = self.df.withColumn("Is_in_range", 
+                   when(self.df[column]>=lower, True)
+                  .when(self.df[column].isNull(), F.lit(None).cast('boolean'))
+                  .otherwise(False))
+                return self.df
+            elif upper is not None and lower is None:
+                self.df = self.df.withColumn("Is_in_range", 
+                   when(self.df[column]<=upper, True)
+                  .when(self.df[column].isNull(), F.lit(None).cast('boolean'))
+                  .otherwise(False))
+                return self.df
+        else:
+            print(f'The column {column} is not a numeric column')
+            return self.df
+        return self.df

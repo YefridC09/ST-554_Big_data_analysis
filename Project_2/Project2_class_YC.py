7553c2c1f2c62cfa7f3082a2ce91d1dc68e03825
YefridC09 <yacordob@ncsu.edu>
2026-03-18 15:20:26 -0400

    Completed validation methods

--- Project_2/Project2_class_YC.py
+++ Project_2/Project2_class_YC.py
@@ -36,32 +36,92 @@ class SparkDataCheck:
     
     #Validation methods creation
     
+    #Creating a column of boolean that verify if the values of a numeric column are in the range of a what a user especifies.
     def create_range_boolean(self, column, lower = None, upper = None):
-        dict_dtypes = dict(self.df.dtypes)
-        col_type = dict_dtypes.get(column)
         
+        #A dictionary with the columns and their type is created
+        dict_dtypes = dict(self.df.dtypes) 
+        
+        #The type of the column on which it is going to be verified is extracted and stored in a new variable
+        col_type = dict_dtypes.get(column) 
+        
+        #A list with the numeric types of variables is created
         num_types = ['float', 'int', 'longint', 'bigint', 'double', 'integer']
         
+        #We verify if the column is introduced, and that its type is numeric
         if col_type is not None and col_type.lower() in num_types:
+            
+            #If it is numeric, we check wheter both limits have been input to the method
             if lower is not None and upper is not None:
+                
+                #If both limits were input, will check for if they are in the range then create a boolean column indicating which fall in the range
                 self.df = self.df.withColumn("Is_in_range", 
-                   when((self.df[column]>=lower & self.df[column]<=upper), True)
-                  .when(self.df[column].isNull(), F.lit(None).cast('boolean'))
+                  F.when(self.df[column].isNull(), None)
+                  .when((self.df[column]>=lower & self.df[column]<=upper), True)
                   .otherwise(False))
-                return self.df
+              
+            
             elif lower is not None and upper is None:
+                
+                #If just the lower limit is provided, will check just that bound and generate the boolean column
                 self.df = self.df.withColumn("Is_in_range", 
-                   when(self.df[column]>=lower, True)
-                  .when(self.df[column].isNull(), F.lit(None).cast('boolean'))
+                  F.when(self.df[column].isNull(), None)
+                  .when(self.df[column]>=lower, True)
                   .otherwise(False))
-                return self.df
+             
+            
             elif upper is not None and lower is None:
-                self.df = self.df.withColumn("Is_in_range", 
-                   when(self.df[column]<=upper, True)
-                  .when(self.df[column].isNull(), F.lit(None).cast('boolean'))
+                
+                #If just the upper limit is provided, will check just that bound and generate a boolean column that meet the conditions
+                self.df = self.df.withColumn("Is_in_range",                 
+                  F.when(self.df[column].isNull(), None)
+                  .when(self.df[column]<=upper, True)
                   .otherwise(False))
-                return self.df
+               
+        
         else:
+            
+            #If the column is not numeric, will print the message and return the dataframe without boolean column
             print(f'The column {column} is not a numeric column')
             return self.df
         return self.df
+    
+    #A method that checks if each value in a string column falls within a user specified set of levels and returns the dataframe with an appended column of Boolean values.
+    def create_string_boolean(self, column, str_list: list):
+        dict_dtypes = dict(self.df.dtypes) 
+        
+        #The type of the column on which it is going to be verified is extracted and stored in a new variable
+        col_type = dict_dtypes.get(column) 
+             
+        #We verify if the column is introduced, and that its type string
+        if col_type is not None and col_type.lower() == "string":
+            
+            #If the column is string type we check if the values supplied are in the column and create a boolean column
+            self.df = self.df.withColumn(f"Is_in_{column}", 
+                F.when(self.df[column].isNull(), None)
+                 .when(self.df[column].isin(str_list), True)
+                 .otherwise(False))
+         
+        else:
+            
+            #If the column is not numeric, will print the message and return the dataframe without boolean column
+            print(f'The column {column} is not a string column')
+
+        return self.df
+    
+    #A method that check if a eachvalue in a column is missing(NULL specifically) and create a column of boolean values
+    def create_missing(self, column):
+        
+        #Checking if the value doesn't exist, then the boolean for that row will be true
+        self.df = self.df.withColumn("Is_missing", 
+            F.when(self.df[column].isNull(), True)
+            .otherwise(False))
+        return self.df
+    
+    #Methods for summaries
+    
+    #Method to report min and max of a numeric column supplied by the user
+    def get_min_max(self, column = None, group = None):
+        
+        
+    
